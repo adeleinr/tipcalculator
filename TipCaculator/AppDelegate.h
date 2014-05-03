f551cab5666329a3bdcd5e3e9d924b2204d3fd4f
adelein rodriguez <adeleinr@gmail.com>
2014-05-02 21:53:25 -0700

    Initial Commit

---
+++ TipCaculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCaculator
+//
+//  Created by Adelein Rodriguez on 5/2/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
